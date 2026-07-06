f87763090c22b3b2b760e5b6eca09af88576b786
dio-min <villaloboskristinesheilam@gmail.com>
2026-07-06 15:14:24 +0800

    : edited external api

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,13 @@ const userRoutes = require('./src/routers/userRoutes');
 const packageRoutes = require('./src/routers/packageRoutes');
 const bookingRoutes = require('./src/routers/bookingRoutes');
 const transactionRoutes = require('./src/routers/transactionRoutes');
-const externalRoutes = require('./src/routers/externalRoutes');
+
+
+const Booking = require('./src/models/booking');
+const Package = require('./src/models/packages');
+const User = require('./src/models/user');
+const Destination = require('./src/models/destination');
+const Transaction = require('./src/models/transaction');
 
 const PORT = process.env.PORT || 5000;
 
@@ -26,8 +32,50 @@ app.use('/api/users', userRoutes);
 app.use('/api/packages', packageRoutes);
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/transactions', transactionRoutes);
-app.use('/api/external', externalRoutes);
 
+
+const isAuthorizedInternalRequest = (req) => {
+  const apiKey = req.headers['x-api-key'];
+  return apiKey && apiKey === process.env.INTERNAL_API_KEY;
+}
+
+app.get("/api/external/summary", async (req, res) => {
+  
+
+   if (!isAuthorizedInternalRequest(req)) {
+    return res.status(401).json({ success: false, message: "Unauthorized" });
+  }
+
+  const totalBookings = await Booking.countDocuments();
+  const totalPackages = await Package.countDocuments();
+  const totalTravelerUsers = await User.countDocuments({ role: 'traveler' });
+  const totalDestination = await Destination.countDocuments();
+  
+  const revenueResult = await Transaction.aggregate([
+    { $group: { _id: null, totalAmount: { $sum: "$amount" } } }
+  ]);
+  const totalRevenue = revenueResult[0]?.totalAmount || 0;
+
+  const mostTrendingPackageResult = await Booking.aggregate([
+    { $group: {  packageName: { $first: "$packageName" }, count: { $sum: 1 } } },
+    { $sort: { count: -1 } },
+    { $limit: 1 }
+  ]);
+
+  const mostTrendingPackage = mostTrendingPackageResult[0];
+
+  res.json({ totalBookings, totalDestination, totalTravelerUsers, totalPackages, totalRevenue, mostTrendingPackage,  });
+});
+
+app.get("/api/external/transactions", async (req, res) => {
+  // API key check
+ if (!isAuthorizedInternalRequest(req)) {
+    return res.status(401).json({ success: false, message: "Unauthorized" });
+  }
+
+  const transactions = await Transaction.find().sort({ transactionDate: -1, _id: -1 });
+  res.status(200).json(transactions);
+});
 
 
 
--- backend/src/routers/externalRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { summaryStats, getTransactions } = require('../controllers/externalController');
-
-router.get('/summary', summaryStats);
-router.get('/transactions', getTransactions);
-
-module.exports = router;
